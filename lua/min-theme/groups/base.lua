bd603a01cfacc36118a2ac9d0ce905205c7d354c
Flavio Del Grosso <flaviodelgrosso@live.it>
2026-02-18 20:23:38 +0100

    update

--- lua/min-theme/groups/base.lua
+++ lua/min-theme/groups/base.lua
@@ -29,9 +29,9 @@ function M.get(colors, config)
     ThemePrimaryActive = { fg = colors.primary_active },
     ThemePrimaryActiveBold = { fg = colors.primary_active, bold = config.bold },
     ThemePrimarySign = config.transparent_mode and { fg = colors.primary, reverse = config.invert_signs }
-      or { fg = colors.primary, bg = colors.bg1, reverse = config.invert_signs },
+      or { fg = colors.primary, bg = colors.bg0, reverse = config.invert_signs },
     ThemeSecondarySoftSign = config.transparent_mode and { fg = colors.secondary_soft, reverse = config.invert_signs }
-      or { fg = colors.secondary_soft, bg = colors.bg1, reverse = config.invert_signs },
+      or { fg = colors.secondary_soft, bg = colors.bg0, reverse = config.invert_signs },
     ThemePrimaryUnderline = { undercurl = config.undercurl, sp = colors.primary },
     ThemeSecondaryActiveUnderline = { undercurl = config.undercurl, sp = colors.secondary_active },
     ThemePrimaryMutedUnderline = { undercurl = config.undercurl, sp = colors.primary_muted },
@@ -39,27 +39,27 @@ function M.get(colors, config)
     ThemePrimaryActiveUnderline = { undercurl = config.undercurl, sp = colors.primary_active },
     ThemeWarning = { fg = colors.warning },
     ThemeWarningSign = config.transparent_mode and { fg = colors.warning, reverse = config.invert_signs }
-      or { fg = colors.warning, bg = colors.bg1, reverse = config.invert_signs },
+      or { fg = colors.warning, bg = colors.bg0, reverse = config.invert_signs },
     ThemeWarningUnderline = { undercurl = config.undercurl, sp = colors.warning },
     ThemeInfo = { fg = colors.info },
     ThemeInfoSign = config.transparent_mode and { fg = colors.info, reverse = config.invert_signs }
-      or { fg = colors.info, bg = colors.bg1, reverse = config.invert_signs },
+      or { fg = colors.info, bg = colors.bg0, reverse = config.invert_signs },
     ThemeInfoUnderline = { undercurl = config.undercurl, sp = colors.info },
     ThemeSuccess = { fg = colors.success },
     ThemeSuccessSign = config.transparent_mode and { fg = colors.success, reverse = config.invert_signs }
-      or { fg = colors.success, bg = colors.bg1, reverse = config.invert_signs },
+      or { fg = colors.success, bg = colors.bg0, reverse = config.invert_signs },
     ThemeSuccessUnderline = { undercurl = config.undercurl, sp = colors.success },
     ThemeGitAdded = { fg = colors.git_added },
     ThemeGitAddedBold = { fg = colors.git_added, bold = config.bold },
     ThemeGitAddedSign = config.transparent_mode and { fg = colors.git_added, reverse = config.invert_signs }
-      or { fg = colors.git_added, bg = colors.bg1, reverse = config.invert_signs },
+      or { fg = colors.git_added, bg = colors.bg0, reverse = config.invert_signs },
     ThemeGitModifiedBold = { fg = colors.git_modified, bold = config.bold },
     ThemeGitModifiedSign = config.transparent_mode and { fg = colors.git_modified, reverse = config.invert_signs }
-      or { fg = colors.git_modified, bg = colors.bg1, reverse = config.invert_signs },
+      or { fg = colors.git_modified, bg = colors.bg0, reverse = config.invert_signs },
     ThemeGitDeleted = { fg = colors.git_deleted },
     ThemeGitDeletedBold = { fg = colors.git_deleted, bold = config.bold },
     ThemeGitDeletedSign = config.transparent_mode and { fg = colors.git_deleted, reverse = config.invert_signs }
-      or { fg = colors.git_deleted, bg = colors.bg1, reverse = config.invert_signs },
+      or { fg = colors.git_deleted, bg = colors.bg0, reverse = config.invert_signs },
   }
 end
 
